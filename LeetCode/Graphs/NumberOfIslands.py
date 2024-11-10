9ed1ebed89704e0a11adfb292324b018d946bed1
Saaji <saajid.aliyar@outlook.com>
2024-11-10 18:19:38 -0500

    using DFS

--- LeetCode/Graphs/NumberOfIslands.py
+++ LeetCode/Graphs/NumberOfIslands.py
@@ -1,26 +1,26 @@
-class Solution():
-    
-    def numIslands(self, grid):
-        
+class Solution:
+    def dfs(self, grid, row,col):
+        if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] != "1":
+            return
+        grid[row][col] = "0"
+        lst = [(row-1, col), (row+1, col) , (row, col+1), (row, col-1) ]
+        for r, c in lst:
+                self.dfs(grid, r, c)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
         numIslands = 0
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 1:
+                if grid[row][col] == "1":
                     self.dfs(grid, row,col)
-                numIslands+=1 
+                    numIslands+=1 
         return numIslands
     
-    def dfs(self, grid, row,col):
-        grid[row][col] = "0"
-        lst = [(row-1, col), (row+1, col) , (row, col+1), (row, col-1) ]
-        for r, c in lst:
-            if grid[r][c] == "1" or r >= 0 or c >= 0 or r <= len(grid) or c <= len(grid[0]):
-                self.dfs(grid, row, col)
+
     
         
-
 
 def main():
     s = Solution()
