a548b1e2e550948e04a7186410552cbbf84b377c
Saaji <saajid.aliyar@outlook.com>
2024-11-10 23:01:20 -0500

    don't understand, try to find different sol

---
+++ LeetCode/Graphs/CloneGraph.py
@@ -0,0 +1,42 @@
+
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldToNew = {}
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
+
+            return copy
+        
+        return dfs(node) if node else None
+
+
+def main():
+    s = Solution()
+    
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+
+    node1.neighbors = [node2, node4]
+    node2.neighbors = [node1, node3]
+    node3.neighbors = [node2, node4]
+    node4.neighbors = [node1, node3]
+
+    print(s.cloneGraph(node1))
+
+
+if __name__ == "__main__":
+    main()
