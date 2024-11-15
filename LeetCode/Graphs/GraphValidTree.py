f3230d80862c5702f30fc573f0a2ec2be2781d2d
Saaji <saajid.aliyar@outlook.com>
2024-11-15 06:40:31 -0500

    premium question, using adjacency list

--- LeetCode/Graphs/GraphValidTree.py
+++ LeetCode/Graphs/GraphValidTree.py
@@ -1,29 +1,29 @@
 from collections import defaultdict
 
+class Solution(object):
+    def validTree(self, n,edges):
+        if not n:
+            return True
 
-def validTree(self, n , edges):
-    if not n:
-        return True
+        graph = defaultdict(list)
 
-    adj = defaultdict(list)
-    
-
-    for n1, n2 in edges:
-        adj[n1].append(n2)
-        adj[n2].append(n1)
-
-    visit = set()
-    def dfs(i,prev):
-        if i in visit:
-            return False
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
         
-        visit.add(i)
-        for j in adj[i]:
-            if j==prev:
-                continue
-            if not dfs(j,i):
+        visited = set()
+        def dfs(i,prev):
+            if i in visited:
                 return False
-        return True
-    
+            visited.add(i)
+            for j in graph[i]:
+                if j==prev:#we're in the same loop? 
+                    continue
+                if not dfs(j, i):
+                    return False
+            return True
+                    
+        return dfs(0, -1) and n == len(visited)
 
-    return dfs(0,-1) and n == len(visit)
+s = Solution()
+print(s.validTree(5, [[0, 1], [0, 2], [0, 3], [1, 4]]))
