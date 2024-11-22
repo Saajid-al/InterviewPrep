990ce212db73230222b85bc7976bec134c901127
Saaji <saajid.aliyar@outlook.com>
2024-11-21 21:18:20 -0500

    redo

--- LeetCode/ArraysandHashMaps/SortCharactersByFrequency.py
+++ LeetCode/ArraysandHashMaps/SortCharactersByFrequency.py
@@ -2,12 +2,17 @@ from collections import Counter, defaultdict
 
 
 class Solution:
-    def frequencySort(self, s: str) -> str:
+    def frequencySort(self, s):
         n = Counter(s)
         hashmap = defaultdict(list)
-        for char, cnt in n:
+        for char, cnt in n.items():
             hashmap[cnt].append(char)
-        print(n)
+
+        for i in range(len(s), 0 , -1):
+            for c in hashmap[i]:
+                res += c * i
+        return res            
+
 
 s = Solution()
 print(s.frequencySort("tree"))
