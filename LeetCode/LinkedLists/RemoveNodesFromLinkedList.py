40d9e2b7516d6ec926ad4b61fd12741a91ce6b68
Saaji <saajid.aliyar@outlook.com>
2024-12-21 19:59:21 -0500

    L code

--- LeetCode/LinkedLists/RemoveNodesFromLinkedList.py
+++ LeetCode/LinkedLists/RemoveNodesFromLinkedList.py
@@ -1,22 +1,51 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def removeNodes(self, head):
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyNode = ListNode(0)
+        dummyNode.next = head
         curr = head
-        while(curr.next)
-            while(curr.val > curr.next.val):
-                
-    
-    def printLinkedList(self, head):
-        curr = head
-
+        stack = []
+        prev = None
         while(curr):
-            print(curr.val, end="->" if curr.next else "\n" )
-            curr = curr.next
-        
+            stack.append(curr.val)
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        maxElement = prev.val
+        previousDummy = ListNode(0)
+        previousDummy.next = prev
+        dummy = ListNode(0)
+        dummy.next = prev
+
+        while(prev): #we found a max Element
+            if prev.val>=maxElement:
+                maxElement = prev.val
+                previousDummy = prev
+                prev = prev.next
+            else:
+                previousDummy.next = prev.next
+                prev = prev.next
+                
+
+        #reverse the list once again to return properly
+        curr2 = dummy.next
+        previous = None
+        while(curr2):
+            nxt = curr2.next
+            curr2.next = previous
+            previous = curr2
+            curr2 = nxt
+
+
+        return previous
+
+
 def main():
     s = Solution()
 
